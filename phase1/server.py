1b07ff4628d3a98f13d280a8a180a8c8d233a389
Aditya Manoharan <adityavazhipokkil@gmail.com>
2018-11-07 00:50:40 -0500

    Initial commit

---
+++ phase1/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from pymongo import MongoClient
+import requests, json
+from subprocess import Popen, PIPE
+
+app = Flask(__name__)
+
+# Making connection to locally hosted MongoDB
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client.roadtrip_buddy
+# weather_c = db.weather_c
+# map_c = db.map_c
+
+@app.route('/')
+def home():
+    return render_template('frontend/index.html')
+
+@app.route('/runcode', methods=['POST'])
+# API to handle POST request for Maps sent by client
+def runcode():
+    data =json.loads(request.data)
+
+    # Code below
+    print(data['code'])
+
+    # Subprocess run code
+    f = open("test.py", "w")
+    f.write(data['code'])
+    f.close()
+    process = Popen(['python', 'test.py'], stdout=PIPE, stderr=PIPE)
+
+    stdout, stderr = process.communicate()
+
+    print(type(stdout))
+    
+    returnval = '{"key":"'+stdout[:-1]+'"}'
+    print(returnval)
+    return jsonify(stdout)
+
+if __name__ == '__main__':
+    app.run(debug=True)
